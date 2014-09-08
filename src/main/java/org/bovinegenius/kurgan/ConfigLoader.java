51a452972e5047a8c9355be7fc937e4c85b49618
Walter Tetzner <walter@waltertetzner.net>
2014-09-07 21:07:01 -0400

    Added loadYaml and loadAllYaml overloads to read from an InputStream.

--- src/main/java/org/bovinegenius/kurgan/ConfigLoader.java
+++ src/main/java/org/bovinegenius/kurgan/ConfigLoader.java
@@ -3,6 +3,7 @@ package org.bovinegenius.kurgan;
 import static java.lang.String.format;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +31,40 @@ public class ConfigLoader {
     }
 
     @SuppressWarnings("unchecked")
+    public static <T> Iterable<T> loadAllYaml(Class<T> cls, String location, InputStream input) {
+        try {
+            ConfigType type = ConfigType.Conversions.asConfigType(cls);
+            Iterable<Node> nodes = YamlUtils.readNodes(location, input);
+            List<T> results = new ArrayList<>();
+            for (Node node : nodes) {
+                type.typeCheck(node);
+                results.add((T)type.coerce(node));
+            }
+            return results;
+        } catch (IOException e) {
+            throw new ConfigException(format("Failed to load config from %s", location), e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
     public static <T> T loadYaml(Class<T> cls, String location) {
         try {
             ConfigType type = ConfigType.Conversions.asConfigType(cls);
             Node node = YamlUtils.readNode(location, location);
+            type.typeCheck(node);
+            return (T)type.coerce(node);
+        } catch (IOException e) {
+            throw new ConfigException(format("Failed to load config from %s", location), e);
+        } catch (ComposerException e) {
+            throw new ConfigException(e.getMessage(), e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T loadYaml(Class<T> cls, String location, InputStream input) {
+        try {
+            ConfigType type = ConfigType.Conversions.asConfigType(cls);
+            Node node = YamlUtils.readNode(location, input);
             type.typeCheck(node);
             return (T)type.coerce(node);
         } catch (IOException e) {
